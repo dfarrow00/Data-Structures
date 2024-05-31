e67a5ca3607eb0678c3f904d093bd2db6cca554d
Dominic Farrow <dominicfarrow@hotmail.com>
2024-05-31 09:49:23 +0100

    Implemented Queue data structure

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,22 @@
-public class Queue {
+//Queue implementation
+public class Queue<E> {
+    LinkedList<E> queue = new LinkedList<>();
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public int length(){
+        return queue.length();
+    }
+
+    public void add(E value){
+        queue.insert(queue.length(), value);
+    }
+
+    public E remove(){
+        E value = queue.get(0);
+        queue.delete(0);
+        return value;
+    }
 }
